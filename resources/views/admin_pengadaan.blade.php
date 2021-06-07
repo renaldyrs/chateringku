eb6c1f3e17f919295051d0c4a2bcec040ec86330
Shiruishi <51904233+Shiruishi@users.noreply.github.com>
2021-06-07 20:22:33 +0700

    tampilan admin, belum jalan wkwkwk

--- resources/views/admin_pengadaan.blade.php
+++ resources/views/admin_pengadaan.blade.php
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
